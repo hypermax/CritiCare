f6ee86b6e3cfbceecef35b19463a1bb399e23d82
Mostafa ALILI <fxmustapha@gmail.com>
2026-09-06 12:50:51 +0100

    Update condition for displaying active stay banner

--- resources/views/patients/show.blade.php
+++ resources/views/patients/show.blade.php
@@ -36,7 +36,7 @@
             @endif
 
             {{-- Bandeau séjour actif --}}
-            @if($activeStay)
+            @if(! $deceasedStay && ! $activeStay)
                 <div class="bg-green-50 border border-green-300 rounded-lg p-4 flex items-center justify-between">
                     <div>
                         <p class="font-semibold text-green-800">
