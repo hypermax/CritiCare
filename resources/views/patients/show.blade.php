143df5a0d33781ea41acd4e2c78fe688a5ed4c24
Mostafa ALILI <fxmustapha@gmail.com>
2026-09-06 19:50:41 +0100

    Réadmission pré-remplie : IPP passé depuis la fiche patient + préremplissage auto du formulaire

--- resources/views/patients/show.blade.php
+++ resources/views/patients/show.blade.php
@@ -114,7 +114,7 @@
                     </div>
                     @if(! $deceasedStay && ! $activeStay)
                         <div class="mt-6">
-                            <a href="{{ route('admissions.create') }}"
+                            <a href="{{ route('admissions.create', ['record_number' => $patient->record_number]) }}"
                                class="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-500">
                                 + Nouvelle admission
                             </a>
